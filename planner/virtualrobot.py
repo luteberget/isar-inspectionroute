b3f69369a1c3368ad1a56b2dfd0c489fade58811
Synne Fossøy <synne.fossoy@sintef.no>
2023-03-23 09:13:33 +0100

    Virtualrobot utesta

--- planner/virtualrobot.py
+++ planner/virtualrobot.py
@@ -3,15 +3,15 @@
 from typing import List, Tuple
 
 from model import Location, RobotState, calculate_distance,calculate_line
-from robotbase import RobotBase 
+from robotbase import RobotBase, TaskStatus
    
 class VirtualRobot(RobotBase):
     current_state = None
     waypoints = None
     wp_step = 0
-    distance_map = None
     next_wps = []
     step_n = 10
+    events = []
     def __init__(self,initial_state) -> None:
         super().__init__()
         self.current_state = initial_state
@@ -25,18 +25,25 @@ class VirtualRobot(RobotBase):
         if len(self.waypoints) <= self.wp_step:
             print("Finished with given waypoints")
             return self.current_state
-        
-        elif self.current_state.location != self.waypoints[self.wp_step](1):
+        elif self.current_state.location == self.waypoints[self.wp_step](1):
+            # selecting new goal
+            events += [TaskStatus(self.waypoints[self.wp_step](0),True)]
+            self.wp_step = self.wp_step+1
+            if len(self.waypoints) > self.wp_step:
+                self.next_wps = calculate_line(self.current_state.location,self.waypoints[self.wp_step](1),self.step_n)
+        elif len(self.next_wps) <= 0:
             # moving towards goal
             distance = calculate_distance(self.current_state.location,self.next_wps[0])
             self.current_state.location = self.next_wps[0]
             self.next_wps.pop(0)
             self.current_state.battery_constraint.remaining_distance -= distance
-
-        elif self.current_state.location == self.waypoints[self.wp_step](1):
-            # selecting new goal
-            self.wp_step = self.wp_step+1
-            if len(self.waypoints) > self.wp_step:
-                self.next_wps = calculate_line(self.current_state.location,self.waypoints[self.wp_step](1),self.step_n)
-        
+        else:
+            events += [TaskStatus(self.waypoints[self.wp_step](0),False)]
+            # Now : do not continoue on waypoints when one is failed.
+            # Possible : continues and let's the 
         return self.current_state
+    
+    def get_event(self):
+        evs = self.events
+        self.events = []
+        return evs
